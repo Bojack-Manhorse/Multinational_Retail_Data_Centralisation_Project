87b3baa640364467415f97a47a5ffbd5edbc01bc
Ibrahim <ibs@pop-os.localdomain>
2024-07-02 12:05:28 +0100

    Updated readme, need to clean up project

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Multinational Retail Data Centralisation project
 
-This project contains code which extracts pandas dataframes from the following sources:
+This project creates a STAR-scheme database based on sales data from a retail store consisting of a large number of stores. We create python scripts to extract and clean data from various sources, upload them to a PostgreSQL database and perform various SQL queries.
+
+The sources we extract data from include the following:
 
 - An AWS Database
 - A pdf file containing a table
@@ -8,17 +10,35 @@ This project contains code which extracts pandas dataframes from the following s
 - A `.csv` file stored in an AWS s3 bucket
 - A json file
 
-The dataframes are then uploaded to a postgresql database and various sql queries are performed on the database.
-
 ## Instructions
 
 ### Installation
 
-Run `git clone Bojack-Manhorse/Multinational_Retail_Data_Centralisation_Project` in the terminal, then `cd Multinational_Retail_Data_Centralisation_Project`.
+First ensure python is installed on your system, along with java (to run tabula).
+
+To clone the repository, run the following in a terminal:
+
+```
+git clone Bojack-Manhorse/Multinational_Retail_Data_Centralisation_Project
+``` 
+
+Then move into the porject directory:
+
+```
+cd Multinational_Retail_Data_Centralisation_Project
+```
+
+Finally install the required python libraries:
+
+```
+pip install -r requirements.txt
+```
 
 ### Setup
 
-Within the same terminal as above, first run `pip install -r requirements.txt`. Then create two files called `db_creds.yaml` and `local_db_creds.yaml` in the project folder. Fill them out as done in `credentials_template.yaml`.
+Within the project directory, create two files called 
+
+`db_creds.yaml` and `local_db_creds.yaml` in the project folder. Fill them out as done in `credentials_template.yaml`.
 - `db_creds.yaml` will contain the authentication for the database you wish to extract rds tables from.
 - `local_db_creds.yaml` will contain the authentication for the database you wish to upload all the tables to.
 
@@ -30,7 +50,7 @@ The file structure should look like this:
 
 Run `python3 data_extraction.py` in the terminal.
 
-### Packages used
+## Packages used
 
 - `boto3`
 - `pandas`
@@ -38,7 +58,3 @@ Run `python3 data_extraction.py` in the terminal.
 - `tabula`
 - `sqlalchemy`
 - `yaml`
-
-### License information
-
-???
--- main.py
+++ main.py
@@ -1,68 +1,20 @@
-import boto3
 import data_cleaning
+import data_extraction
 import database_utils
-import pandas as pd
-import requests
-import tabula
+
+my_connection = database_utils.DatabaseConnector()
+my_extractor = data_extraction.DataExtractor()
+my_hoover = data_cleaning.DataCleaning()
 
 local_database_creds = "local_db_creds.yaml"
 external_database_creds = "db_creds.yaml"
 
-class DataExtractor:
-    """
-        Used to extract data from different data sources
-    """
-    def read_rds_table(self, database_connector, table_name):
-        """
-            Returns a pandas dataframe of 'table_name' in 'database_connector'
-        """
-        return pd.read_sql_table(table_name, database_connector.init_db_engine(external_database_creds),index_col='index')
-    
-    def retrieve_pdf_data(self, link):
-        """
-            Returns a list of pandas dataframes from a pdf file
-        """
-        return pd.concat(tabula.read_pdf(link, pages='all'), ignore_index=True)
-    
-    def list_number_of_stores(self, link_1, header):
-        """
-            Retrieves data from 'end_point' with authentication from 'header'.
-        """
-        return requests.get(link_1, headers=header)
-
-    def retrieve_stores_data(self, link_1, link_2, header):
-        """
-            Uses 'list_number_of_stores' to retrieve data from all stores.
-        """
-        number_of_stores = self.list_number_of_stores(link_1, header).json()['number_stores']
-        list_of_stores = []
-        for i in range(0,number_of_stores):
-            list_of_stores.append(self.list_number_of_stores(link_2 + str(i), header).json())
-        output = pd.DataFrame(list_of_stores)
-        return output
-    
-    def extract_from_s3(self, s3_address):
-        """
-            Extracts a dataframe from a csv in an s3 Bucket from s3_address
-        """
-        s3 = boto3.client('s3')
-        list = s3_address.split('/')
-        s3.download_file(list[2], list[3], list[3])
-        return pd.read_csv(list[3])
-    
-    def read_json(self, link):
-        return pd.read_json(link) #Should this be in a context manager?
-    
-my_connection = database_utils.DatabaseConnector()
-my_extractor = DataExtractor()
-my_hoover = data_cleaning.DataCleaning()
-    
 def upload_user_data():
     df_raw = my_extractor.read_rds_table(my_connection, 'legacy_users')
     df_cleaned = my_hoover.clean_user_data(df_raw)
     my_connection.upload_to_db(df_cleaned, 'dim_users', local_database_creds)
 
-#upload_user_data()
+upload_user_data()
 
 def upload_card_data():
     pdf_link = 'https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf'
@@ -114,4 +66,4 @@ def all_functions():
     upload_order_data()
     upload_events_data()
 
-all_functions()
+#all_functions()
